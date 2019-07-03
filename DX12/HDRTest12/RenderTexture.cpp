c1b7e17dbc34f36c40237eb9273b13f723e47736
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-07-03 12:34:48 -0700

    Fixed -Wpragma-once-outside-header warning

--- DX12/HDRTest12/RenderTexture.cpp
+++ DX12/HDRTest12/RenderTexture.cpp
@@ -11,8 +11,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //-------------------------------------------------------------------------------------
 
-#pragma once
-
 #include "pch.h"
 #include "RenderTexture.h"
 
