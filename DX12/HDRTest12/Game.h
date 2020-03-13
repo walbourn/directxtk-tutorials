ed11fbba04f7af86f217cb9e70bf960fb9add1f1
Chuck Walbourn <chuckw@windows.microsoft.com>
2020-03-13 14:28:47 -0700

    Minor code review

--- DX12/HDRTest12/Game.h
+++ DX12/HDRTest12/Game.h
@@ -25,8 +25,8 @@ public:
     void Tick();
 
     // IDeviceNotify
-    virtual void OnDeviceLost() override;
-    virtual void OnDeviceRestored() override;
+    void OnDeviceLost() override;
+    void OnDeviceRestored() override;
 
     // Messages
     void OnActivated();
