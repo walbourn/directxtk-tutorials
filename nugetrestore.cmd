f25b86554583d9a0784943f8c19f321c792b3931
Chuck Walbourn <chuckw@microsoft.com>
2022-11-19 17:59:22 -0800

    D3DX12 updated for Agility SDK 1.608

---
+++ nugetrestore.cmd
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+set NUGET=c:\util\nuget.exe
+if NOT exist %NUGET% goto nuerror
+if %VisualStudioVersion%.==. goto deverr
+echo .
+pushd DX11
+for /D %%1 in (*) do (pushd %%1
+if exist "%%1.sln" (
+%NUGET% restore "%%1.sln") 
+popd)
+popd
+pushd DX12
+for /D %%1 in (*) do (pushd %%1
+if exist "%%1.sln" (
+%NUGET% restore "%%1.sln")
+popd)
+popd
+goto :eof
+:nuerror
+echo ERROR cannot find %NUGET%
+goto :eof
+:deverr
+echo ERROR Run this script from a Visual Studio Developer Command Prompt
+
